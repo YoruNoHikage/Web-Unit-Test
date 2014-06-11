92a0072a6d367ba4748eba8a74978d2352055458
YoruNoHikage <yorunohikage@gmail.com>
2014-06-11 10:38:44 +0200

    Startup legitimizer and index

--- Views/footer.php
+++ Views/footer.php
@@ -1,14 +1,22 @@
             <div class="clearfix"></div>
             <hr/>
             <footer class="row">
-                <p class="col-md-12 text-center">WTFPL - do What The Fuck you want with the code !</p>
+                <div class="col-md-12 text-center">
+                    <script src="http://thestartuplegitimizer.com/js/legitimizeme.min.js?pubs=bu-tc-hu-be-bf-te-cn-ny-bb"></script>
+                    <style>
+                        .legitimizer-container {
+                            width: 100%;
+                            text-align: center;
+                        }
+                    </style>
+                </div>
             </footer>
         </div>
-        
+
         <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <script>
-      		$('.tip').tooltip({html: true});
+            $('.tip').tooltip({html: true});
         </script>
         <script src="Views/scripts/moment.js"></script>
         <script src="Views/scripts/circles.min.js"></script>
